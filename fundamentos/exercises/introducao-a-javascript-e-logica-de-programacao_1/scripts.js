809ffd1093ef3ea970fb031cd55a3fd386577cd6
k4nl <gustavoh.bh@gmail.com>
2021-05-20 19:47:32 -0300

    adcionando um programa que calcula o salario liquido ( salario bruto - impostos descontados )

--- fundamentos/exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ fundamentos/exercises/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -184,4 +184,37 @@ if (compra < 0 || venda < 0) {
   console.log (lucro * 1000);
 }
 
+// decimo-primeiro exercicio
 
+
+let inss; 
+let ir;
+let bruto = 10000;
+
+if (bruto <= 1556.94) {
+  inss = bruto * 0.08;
+} else if (bruto <= 2594.92) {
+  inss = bruto * 0.09;
+} else if (bruto <= 5189.82) {
+  inss = bruto * 0.11;
+} else {
+  inss = 570.88;
+}
+
+let base = bruto - inss;
+
+if (base <= 1903.98) {
+  ir = 0
+} else if (base <= 2826.65) {
+  ir = (base * 0.075) - 142.8;
+} else if (base <= 3751.05) {
+  ir = (base * 0.15) - 354.8;
+} else if (base <= 4664.68) {
+  ir = (base * 0.225) - 636.13;
+} else {
+  ir = (base * 0.275) - 869.36;
+};
+
+console.log("Seu salário é: " + (base - ir));
+
+// fim dos exercicios!
