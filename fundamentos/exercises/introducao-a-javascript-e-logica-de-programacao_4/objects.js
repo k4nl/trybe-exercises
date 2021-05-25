d056b26d9e0cef9ccf1af4578a3ee9aea45dc415
k4nl <gustavoh.bh@gmail.com>
2021-05-25 12:15:35 -0300

    resolucao exercicio 2

--- fundamentos/exercises/introducao-a-javascript-e-logica-de-programacao_4/objects.js
+++ fundamentos/exercises/introducao-a-javascript-e-logica-de-programacao_4/objects.js
@@ -6,3 +6,8 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 console.log('Bem-vinda, ' + info.personagem) 
+
+// Exercicio 2
+
+info['recorrente'] = 'Sim';
+console.log(info.recorrente);
