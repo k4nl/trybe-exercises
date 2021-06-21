c7b44ba1515c703c09bf6563880546c37139f003
k4nl <gustavoh.bh@gmail.com>
2021-06-21 13:43:09 -0300

    exercicio 1

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/script.js
@@ -0,0 +1,59 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      margherita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`
+}
+
+console.log(customerInfo(order));
+
+order.name = 'Luiz Silva';
+order.order.pizza = {
+  muzzarella: {
+    amount: 1,
+    price: 20,
+  },
+  calabresa: {
+    amount: 1,
+    price: 20,
+  },
+}
+
+const orderModifier = (order) => {
+  return `Olá ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é R$ ${order.order.pizza.muzzarella.price + order.order.pizza.calabresa.price + order.order.drinks.coke.price},00`
+}
+
+console.log(orderModifier(order));
