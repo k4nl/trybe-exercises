28825b55f32fde92f011c63fb2a55521c6b98363
k4nl <gustavoh.bh@gmail.com>
2021-11-29 13:33:05 -0300

    comandos

---
+++ back-end/mongodb-exercises/commands.js
@@ -0,0 +1,8 @@
+// use ("bios")
+// exercicio 1 db.bios.find({ _id: 8});
+// exercicio 2 db.bios.find({ _id: 8}, { _id: 1, name: 1})
+// exercicio 3 db.bios.find({ _id: 8}, { _id: 0, name: 1, birth: 1})
+// exercicio 4 db.bios.find({ "name.first": "John" }).pretty()7
+// exercicio 5 db.bios.find().limit(3)
+// exercicio 6 db.bios.find().limit(2).skip(3)
+
